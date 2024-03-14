e4509ed34ad62540fb2be1d93a421268c60487be
HabtamuFeyera <habtamufeyer02@gmail.com>
2024-03-14 18:11:53 +0300

    add new files to the directory

---
+++ Python_Functions/docstrings function.py
@@ -0,0 +1,13 @@
+def add(a, b):
+    """ Add two arguments
+    Arguments:
+        a: an integer
+        b: an integer
+    Returns:
+        The sum of the two arguments
+    """
+    return a + b
+
+
+total = add(3, 6)
+print(total)
