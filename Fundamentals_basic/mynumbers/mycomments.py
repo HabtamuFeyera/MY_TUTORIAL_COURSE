4158d1210091937df028328a632c88b53f96394a
HabtamuFeyera <habtamufeyer02@gmail.com>
2024-03-14 16:34:46 +0300

    Add simplesone.py and update mycomments.py

---
+++ Fundamentals_basic/mynumbers/mycomments.py
@@ -0,0 +1,13 @@
+#increase price by 5%
+price = price*0.5
+salary = salary * 1.02   # increase salary by 2%
+
+def quicksort():
+  """ sort the list using quicksort algorithm """ #one line docstrings
+...
+def increase(salary, percentage, rating): # multi_line docstrings
+    """ increase salary base on rating and percentage 
+    rating 1 - 2 no increase
+    rating 3 - 4 increase 5%
+    rating 4 - 6 increase 10%
+    """
