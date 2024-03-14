3f09ef6e060f3560dc7dcc5c8afa78b03b673e42
HabtamuFeyera <habtamufeyer02@gmail.com>
2024-03-14 18:34:05 +0300

    add new list file

---
+++ Python_List/listsort.py
@@ -0,0 +1,16 @@
+companies = [('Google', 2019, 134.81),
+             ('Apple', 2019, 260.2),
+             ('Facebook', 2019, 70.7)]
+
+
+# define a sort key
+def sort_key(company):
+    return company[2]
+
+
+
+# sort the companies by revenue
+companies.sort(key=sort_key, reverse=True)
+
+# show the sorted companies
+print(companies)
